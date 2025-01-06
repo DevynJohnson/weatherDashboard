e42a9efc84ccf6c0327ec0025a20bb538aa967af
Devyn Johnson <dljohnson1313@gmail.com>
2025-01-06 17:26:40 -0600

    Working through some of the todo sections of assignment on files in backend section of code. historyServices.ts, weatherService.ts, server.ts, weatherRoutes.ts, htmlRoutes.ts. None are completed, but removing comment // from areas where code is required to be finished.

--- server/src/server.ts
+++ server/src/server.ts
@@ -11,7 +11,9 @@ const PORT = process.env.PORT || 3001;
 
 // TODO: Serve static files of entire client dist folder
 
+
 // TODO: Implement middleware for parsing JSON and urlencoded form data
+
 
 // TODO: Implement middleware to connect the routes
 app.use(routes);
--- server/src/service/historyService.ts
+++ server/src/service/historyService.ts
@@ -1,15 +1,32 @@
 // TODO: Define a City class with name and id properties
+class City {
+  name: string;
+  id: number;
+
+  constructor(name: string, id: number) {
+    this.name = name;
+    this.id = id;
+  }
+}
+
 
 // TODO: Complete the HistoryService class
 class HistoryService {
   // TODO: Define a read method that reads from the searchHistory.json file
-  // private async read() {}
+  private async read(): Promise<City[]> {
+    const cities: City[] = [];
+    return cities;
+  }
   // TODO: Define a write method that writes the updated cities array to the searchHistory.json file
-  // private async write(cities: City[]) {}
+  private async write(cities: City[]): Promise<void> {
+    
+  }
   // TODO: Define a getCities method that reads the cities from the searchHistory.json file and returns them as an array of City objects
-  // async getCities() {}
+  async getCities() {
+    
+  }
   // TODO Define an addCity method that adds a city to the searchHistory.json file
-  // async addCity(city: string) {}
+  async addCity(city: string) {}
   // * BONUS TODO: Define a removeCity method that removes a city from the searchHistory.json file
   // async removeCity(id: string) {}
 }
