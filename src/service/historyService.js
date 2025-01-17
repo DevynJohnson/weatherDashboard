0aa87cf71dc3f513dbaae0e30f9fdc8975ca38cb
Devyn Johnson <dljohnson1313@gmail.com>
2025-01-16 18:39:35 -0600

    Update historyService.js file - create City class and HistoryService class. Add updates to server.js file including middleware to serve static files in client/dist folder, for pasring JSON and urlencoded data, and middleware to connect routes.

--- src/service/historyService.js
+++ src/service/historyService.js
@@ -1,5 +1,23 @@
 // TODO: Define a City class with name and id properties
+class City {
+    constructor(name, id) {
+        this.name = name;
+        this.id = id;
+    }
+}
+
 // TODO: Complete the HistoryService class
 class HistoryService {
+    constructor() {
+        this.apiKey = process.env.API_KEY;
+        this.baseURL = process.env.BASE_URL;
+    }
+
+    async getHistory() {
+        const url = `${this.baseURL}/history?city=${City.name}&id=${City.id}&appid=${this.apiKey}`;
+        const response = await fetch(url);
+        const data = await response.json();
+        return data;
+    }
 }
 export default new HistoryService();
