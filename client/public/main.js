207dce3716f80bd44cf3f18a021f7b7ee33204bc
Devyn Johnson <dljohnson1313@gmail.com>
2025-01-25 19:15:31 -0600

    Fixed issue that was caused by changing from fs.promises.readFile/writeFile to readFile when debugging issue of search history not showing. Changed writeFile to match, no longer having issue with fetchWeather POST request.

--- client/public/main.js
+++ client/public/main.js
@@ -15,19 +15,29 @@ API Calls
 
 */
 const fetchWeather = async (cityName) => {
-    const response = await fetch('/api/weather/', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ cityName }),
-    });
-    const weatherData = await response.json();
-    // console.log('weatherData: ', weatherData);
-    renderCurrentWeather(weatherData[0]);
-    renderForecast(weatherData.slice(1));
+    try {
+        const response = await fetch('/api/weather/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ cityName }),
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const weatherData = await response.json();
+        // console.log('weatherData: ', weatherData);
+        renderCurrentWeather(weatherData[0]);
+        renderForecast(weatherData.slice(1));
+    } catch (error) {
+        console.error('Error fetching weather data:', error);
+    }
 };
-const fetchSearchHistory = async () => {
+
+    const fetchSearchHistory = async () => {
     const response = await fetch('/api/weather/history', {
         method: 'GET',
         headers: {
